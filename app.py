4e2e17b2f9db8bf33b890d8dceb5c835d979cd86
Nancy Singleton <nancy.singleton@softwire.com>
2025-07-10 16:47:51 +0100

    Create next_buses endpoint with dummy data

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/next_buses/<stop_code>')
+def next_buses(stop_code):
+    dummy_buses = ['1001', '1002', '1003']
+    return dummy_buses
+
 @app.route('/')
 def index():
     return 'Hello, Flask!'
