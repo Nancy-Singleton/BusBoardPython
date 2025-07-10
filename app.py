2b1ea7a33a992d9d5686961ddeeedad1e6e2b975
Nancy Singleton <nancy.singleton@softwire.com>
2025-07-10 16:58:21 +0100

    Add real data

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
+import requests
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/next_buses/<stop_code>')
 def next_buses(stop_code):
-    dummy_buses = ['1001', '1002', '1003']
-    return dummy_buses
+    url = f'https://api.tfl.gov.uk/StopPoint/{stop_code}/Arrivals'
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        arrivals = response.json()
+
+        buses = []
+        for bus in arrivals:
+            buses.append({
+                'lineName': bus.get('lineName'),
+                'destinationName': bus.get('destinationName'),
+                'timeToStation_sec': bus.get('timeToStation'),  # seconds until arrival
+                'expectedArrival': bus.get('expectedArrival'),
+            })
+
+        buses.sort(key=lambda x: x['timeToStation_sec'])
+        return buses[:5]
+
+    except requests.exceptions.RequestException as e:
+        return {'error': 'Failed to fetch data from TfL API', 'details': str(e)}, 500
 
 @app.route('/')
 def index():
